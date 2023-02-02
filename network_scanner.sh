33d1f70002c9dd81ad3aa90774775e54ab04323b
SecDet Samurai <79522262+Untouchable17@users.noreply.github.com>
2023-02-03 01:57:51 +0300

    Update network_scanner.sh

--- network_scanner.sh
+++ network_scanner.sh
@@ -7,19 +7,16 @@ else
 	FILE_PATH=$1
 fi
 
-
 COUNT_DOWN_HOSTS=0
 COUNT_UP_HOSTS=0
 
 
 function show_scan_result(){
-
 	COUNT_HOSTS=$(wc -l $FILE_PATH | cut -d ' ' -f1)
 	echo "Scanned hosts: $COUNT_HOSTS"
 	echo "Up hosts: $COUNT_UP_HOSTS"
 	echo "Down hosts: $COUNT_DOWN_HOSTS"
 }
-
 
 function show_error_result(){
 	echo "Script failed for some reason: "
@@ -28,9 +25,7 @@ function show_error_result(){
 	echo "3. File is empty"
 }
 
-
-function send_icmp_request(){
-	
+function send_icmp_request(){	
 	if [[ -n $IP_ADDRESS ]]; then
 		host_status=$(nmap -sn $IP_ADDRESS | grep 'Host is up' | cut -d '(' -f1)
 		if [[ -z $host_status ]]; then
@@ -44,22 +39,17 @@ function send_icmp_request(){
 	fi		
 }
 
-
 function show_dns_name(){
 	dns_name=$(host $IP_ADDRESS)
 	printf "$dns_name\n"
 }
 
-
-function start_script(){
-	
+function start_script(){	
 	if [[ -a $FILE_PATH && -r $FILE_PATH && -s $FILE_PATH ]]; then
-		
 		for ip in $(cat $FILE_PATH); do
 		    IP_ADDRESS=$ip
 		    send_icmp_request
 		done
-		
 		show_scan_result
 	else
 		show_error_result
